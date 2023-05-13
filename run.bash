665cc5d55517ecc43570d7dbced90c6d9640e4a7
sussyAbhi <107315328+sussyAbhi@users.noreply.github.com>
2023-05-13 11:14:42 -0400

    Create run.bash

---
+++ run.bash
@@ -0,0 +1,6 @@
+git clone https://github.com/sussyAbhi/yourrepository.git
+cd yourrepository
+# copy or move your files to this directory
+git add .
+git commit -m "Initial commit"
+git push origin main
